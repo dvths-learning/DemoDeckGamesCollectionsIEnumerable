3f55c2b874bad3e30a350f16f0b4c0df052a224e
Tiago Henrique <tiagohs.dev@gmail.com>
2022-10-12 13:00:16 -0300

    feat: add the truco collection

---
+++ src/DemosDeckGames.ConsoleApp/Models/DeckDefault/TrucoDeck.cs
@@ -0,0 +1,18 @@
+namespace DemosDeckGames.ConsoleApp.Models.DeckDefault;
+
+public class TrucoDeck
+    : DeckDefault
+{
+    protected override void InicializePlaingCards()
+    {
+        base.InicializePlaingCards();
+
+        var invalidCardsForThisDackGame = new[]{8, 9, 10};
+
+        // Cursor que irá passar por cada carta filtrando todos os valores das cartas que não estão nas cartas invalidas para o baralho do truco
+        var trucoCards = CardsCollection.Where(carta => !invalidCardsForThisDackGame.Contains(carta.Value));
+
+        // Materializ as cartas do truco
+        MakeNewDeckGame(trucoCards.ToArray());
+    }
+}

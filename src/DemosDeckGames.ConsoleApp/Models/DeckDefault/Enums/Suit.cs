810166073cd734b88e12f4dbc1cd6e96eebfa96a
Tiago Henrique <tiagohs.dev@gmail.com>
2022-10-12 12:59:23 -0300

    feat: add enum for Suit of the cards.

---
+++ src/DemosDeckGames.ConsoleApp/Models/DeckDefault/Enums/Suit.cs
@@ -0,0 +1,16 @@
+namespace DemosDeckGames.ConsoleApp.Models.DeckDefault.Enums;
+
+public enum Suit
+/*
+   copas =1
+   paus =2
+   ouros =3
+   espadas =4
+
+*/
+{
+    Hearts = 1,
+    Clumbs = 2,
+    Diamonds = 3,
+    Spads = 4
+}
